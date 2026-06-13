1c0cc38ce391f7cb927e05d8addf18620f2db2fb
9mnvdvqjp8-sketch <9mnvdvqjp8@privaterelay.appleid.com>
2026-06-13 05:48:17 -0700

    lesson3

---
+++ lesson3/diferencasimetrike.py
@@ -0,0 +1,10 @@
+from lesson3.unioni import rezultati
+
+set1 = {1,2,3}
+set2 = {3,4,5}
+
+print(set1^set2)
+
+rezultati = set1.diferenca_simetrike(set2)
+
+print(rezultati)
---
+++ lesson3/main.py
@@ -0,0 +1,20 @@
+x = 10
+y=20
+
+print(x<y)
+print(x==y)
+print(X!=Y)
+print(x>=y)
+
+username = "donjezo"
+password = "hello123!"
+
+if username=="donjeta" and password=="hello123!":
+    print("logged in")
+
+person ="Liza"
+if person=="Liza"or person=="Rudina":
+    print("shko te klasa numer 8")
+
+if person=="Liza"and age>=18:
+    print("take the driving license")
