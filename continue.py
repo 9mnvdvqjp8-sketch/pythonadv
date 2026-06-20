8fc4e0b2270a9fd53507ef687290883e41e2fa13
9mnvdvqjp8-sketch <9mnvdvqjp8@privaterelay.appleid.com>
2026-06-20 05:31:16 -0700

    lesson3

---
+++ continue.py
@@ -0,0 +1,6 @@
+personat=["suela","liza","Rudina","Ylli"]
+
+for i in personat
+    if i=="Liza":
+    continue
+    print(i)
---
+++ main.py
@@ -0,0 +1,17 @@
+for i in range(5,10);
+    print(i,"pershendetje")
+
+for e in range(10):
+    print(e,"hi there")
+print("--------")
+for i in range(5,20,5);
+    print(i)
+personat=["suela","Liza","Rudina"]
+
+for i in personat:
+    print(i)
+
+emriDheMbiemri= "Yll Cakiqi"
+
+for i in emriDheMbiemri:
+    print(i)
